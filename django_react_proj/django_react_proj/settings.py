f0ab4bfa64c136f94e5538de0443b1cb0bf14847
Avan <avansardar@outlook.com>
2023-09-11 12:25:25 +0300

    Update readme, minor naming changes, update settings.py to specify psql

--- django_react_proj/django_react_proj/settings.py
+++ django_react_proj/django_react_proj/settings.py
@@ -80,8 +80,8 @@ WSGI_APPLICATION = 'django_react_proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'boilerplate-db', # <==== TODO Change teh name of the database
     }
 }
 
